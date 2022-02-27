de3d549127edd22de45977d1c5b47d8e588f9db6
Elyas <touahria.elyas@outlook.com>
2022-02-27 11:37:23 +0100

    Ts type

--- src/app.ts
+++ src/app.ts
@@ -26,3 +26,24 @@ type MyExclude<T, U> = T extends U ? never : T
 
 // Awaited 
 type MyAwaited<T extends Promise<any>> = T extends Promise<infer U> ? ( U extends Promise<any> ? MyAwaited<U> : U ) : never 
+
+// If
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+// Concat
+type Concat<T extends any[], U extends any[]> = [...T,...U]
+
+// Includes
+type IsEqual<T, U> = T extends U ? (U extends T ? true : false) : false
+type TupleToUnion<T extends readonly any[]> = T[number]
+type Includes<T extends readonly any[], U> = T extends [infer First,...infer Rest] ? ( IsEqual<First,T> extends true ? true : Includes<Rest, U>) : false;
+
+// Push 
+type Push<T extends any[], U> = [...T,U]
+
+// Unshift
+type Unshift<T extends any[], U> = [U,...T]
+
+// Parameters 
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer Args) => any ? Args : never
+
